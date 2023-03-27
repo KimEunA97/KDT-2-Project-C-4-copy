35056ec571ec6978a5d459d8373e140ee7381552
KimEunA97 <kimeuna9712@gmail.com>
2023-03-27 10:31:39 +0900

    api key module

---
+++ module/KEY.js
@@ -0,0 +1,14 @@
+
+
+// https://apis.data.go.kr/1360000/VilageFcstInfoService_2.0/getUltraSrtNcst?serviceKey=bb%2BP09bY3vabZXZRYZxaz8tUfEmSfIx58oQHgWjAfSbbI%2B0NnVcuDVgXENmUzfD68uuICVPx4i2y1bAhTq0nQA%3D%3D&pageNo=1&numOfRows=1000&dataType=JSON&base_date=20210628&base_time=0600&nx=55&ny=127
+
+export const KEYobj = {
+
+  APIreqeustURL: "http://apis.data.go.kr/1360000/VilageFcstInfoService_2.0/getUltraSrtNcst",
+
+  serviceURL: "http://apis.data.go.kr/1360000/VilageFcstInfoService_2.0",
+
+  serviceKEY: "bb%2BP09bY3vabZXZRYZxaz8tUfEmSfIx58oQHgWjAfSbbI%2B0NnVcuDVgXENmUzfD68uuICVPx4i2y1bAhTq0nQA%3D%3D",
+
+
+}
