1d18c18298aaebf2b89c6a18d3f345d83a1b9d7b
KimEunA97 <kimeuna9712@gmail.com>
2023-03-27 10:35:32 +0900

    api key

--- weather.js
+++ weather.js
@@ -2,14 +2,9 @@ import http, { Server } from 'http'
 import url from 'url'
 import path from 'path'
 
+import './module/KEY.js'
+import { KEYobj } from './module/KEY.js';
 
-const APIreqeustURL = "http://apis.data.go.kr/1360000/VilageFcstInfoService_2.0/getUltraSrtNcst";
-
-const serviceURL = "http://apis.data.go.kr/1360000/VilageFcstInfoService_2.0"
-
-const serviceKEY = "bb%2BP09bY3vabZXZRYZxaz8tUfEmSfIx58oQHgWjAfSbbI%2B0NnVcuDVgXENmUzfD68uuICVPx4i2y1bAhTq0nQA%3D%3D"
-
-// https://apis.data.go.kr/1360000/VilageFcstInfoService_2.0/getUltraSrtNcst?serviceKey=bb%2BP09bY3vabZXZRYZxaz8tUfEmSfIx58oQHgWjAfSbbI%2B0NnVcuDVgXENmUzfD68uuICVPx4i2y1bAhTq0nQA%3D%3D&pageNo=1&numOfRows=1000&dataType=JSON&base_date=20210628&base_time=0600&nx=55&ny=127
 
 
 http.createServer(function (request, response) {
@@ -27,7 +22,7 @@ http.createServer(function (request, response) {
 
 }).listen(2080);
 
-_URL = APIreqeustURL + "?" + serviceKEY;
+const _URL = KEYobj.APIreqeustURL + "?" + KEYobj.serviceKEY;
 
 function APIload() {
 
