40e2fd6fd4a84a428aee550a1baeb24053b0b1c4
KimEunA97 <kimeuna9712@gmail.com>
2023-03-31 17:49:27 +0900

    확인해보니 POST를 적극 이용해야겠다.

--- server.js
+++ server.js
@@ -12,6 +12,9 @@ const server = http.createServer(function (request, response) {
 
   if (request.method === 'GET' && request.url === '/') {
 
+
+    
+
     fs.readFile('./show.html', function (error, data) {
 
       if (error) {
@@ -24,13 +27,14 @@ const server = http.createServer(function (request, response) {
 
         response.end(data);
 
-        let buf = Buffer.from(data);
-        let bufStr = Buffer.toString(buf);
-        let bufJson = Buffer.from(bufStr).toJSON();
 
+        let a = Buffer.from(data).toString('utf-8');
+        let b = JSON.stringify(a);
+        console.log(b);
 
+        // let buf = Buffer.from(data);        
+        // buf.toString()
 
-        console.log(a);
         // let parseData = JSON.parse(buf);
         // console.log(parseData);
 
@@ -68,4 +72,5 @@ server.listen(2080, function (error) {
     console.log("서버 구동");
   }
 });
+
 
