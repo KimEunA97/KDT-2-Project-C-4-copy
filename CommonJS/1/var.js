99307c9cc6ca4782c4784c0f242afe059ba3a98a
parksuyeon <clapyyyyy@gmail.com>
2023-04-03 17:30:57 +0900

    책에있는모듈공부하려다가못함

---
+++ CommonJS/1/var.js
@@ -0,0 +1,10 @@
+// const odd = "cjs 홀수입니다."
+// const even = "cjs 짝수입니다."
+// module.exports = {
+//   odd,
+//   even,
+// };
+
+//위처럼 안하고
+exports.odd = 'cjs 홀수입니다.'
+exports.even = 'cjs 짝수입니다.'
---
+++ CommonJS/2/dep-run.js
@@ -0,0 +1,7 @@
+const dep1 = require('./dep1');
+const dep2 = require('./dep2');
+
+dep1();
+dep2();
+
+//순환참조 p104
