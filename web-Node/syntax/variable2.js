7d752b520f2f22ab2cdad342457c5402d47317d5
jungheeeun <jungheeeun272@gmail.com>
2023-04-12 23:19:03 +0900

    변수의 가장 기본적인 형태  /  중복을 제거하자

---
+++ web-Node/syntax/variable2.js
@@ -0,0 +1,13 @@
+//변수의 가장 기본적인 형태  // 중복을 제거하자 
+
+// 각각의 텍스트들이 같은내용인걸 파악하려는데 1억줄의 문자라면?
+var name = 'jungheeeun';  // 변수를 사용했을때의 큰 이익 ! 
+var letter = 'geredo' +name+  'Aute reprehenderit deserunt reprehenderit qui in et minim commodo ipsum enim esse enim duis. Eu consectetur velit elit elit nulla irure. Velit quis '+name+ 'qui tempor proident '+name+' consectetur adipisicing. Qui excepteur fugiat velit commodo anim anim.'
+console.log(letter);
+
+
+
+console.log('Aute reprehenderit deserunt reprehenderit qui in et minim commodo ipsum enim esse enim duis. Eu consectetur velit elit elit nulla irure. Velit quis qui tempor proident consectetur adipisicing. Qui excepteur fugiat velit commodo anim anim.');
+
+
+
