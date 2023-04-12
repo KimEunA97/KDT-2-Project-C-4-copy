bc70fecafd953f5c02859415e2bd15d8f7bb0f0f
jungheeeun <jungheeeun272@gmail.com>
2023-04-12 21:55:09 +0900

    __dirname  개념  & response.end( )응용 해보기

--- web-Node/main.js
+++ web-Node/main.js
@@ -9,7 +9,10 @@ var app = http.createServer(function(request,response){
       return response.writeHead(404);
     }
     response.writeHead(200);
-    response.end(fs.readFileSync(__dirname + url));
+    console.log(__dirname + url);  //콘솔에 표시된다. web-Node/1.html
+    //자바스크립트 코드를 통해서 읽어들이게 될 파일을 만들수 있다. 
+    response.end( 'jungheeeun :' + url);
+    //readFileSync ( node.js ) 그경로의 파일을 읽어서 가져오는것 .  //http://localhost:3000/1.html  => jungheeeun :/1.html
  
 });
 app.listen(3000);
