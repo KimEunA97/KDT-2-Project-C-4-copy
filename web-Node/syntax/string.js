e0bfeb20e13729dab3538d49d47e9f78fc96b061
jungheeeun <jungheeeun272@gmail.com>
2023-04-12 22:39:08 +0900

    이항연산자 , string 결합연산자

---
+++ web-Node/syntax/string.js
@@ -0,0 +1,4 @@
+console.log(1+1); //2 이항연산자( 왼쪽의 값과 오른쪽의 값을 더해서  어떠한 하나의 값을 만든다. )
+console.log('1'+'1');//11  ( 문자열의 양쪽에 위치하게되면 더하기라는 산술연산자가 아닌 결합 연산자로 자동으로 실행한다. )
+console.log('Proident occaecat consequat nostrud amet labore ex esse. Pariatur proident irure veniam elit fugiat non ut. Incididunt excepteur commodo tempor laboris. Ad veniam aliquip consequat non proident non fugiat anim eiusmod tempor occaecat qui. Proident elit deserunt commodo qui adipisicing elit sit fugiat est nostrud pariatur anim proident magna.'.length); //343  
+//계산기가 중요한 것처럼 순간적으로 필요할 때마다 자유롭게 사용하기
