13f6bb66751f9f4bc9d4113f2936de296027639d
KimEunA97 <kimeuna9712@gmail.com>
2023-04-12 14:36:23 +0900

    서버에 데이터 어떤 방식으로 보낼지 깊이 고민 해야봐 할듯.

--- server.js
+++ server.js
@@ -1,8 +1,6 @@
 import fs from 'fs'
-import express from 'express'
+import express from "express";
 const app = express();
-
-
 
 app.get('/', function (req, res) {
 
@@ -20,13 +18,21 @@ app.get('/', function (req, res) {
       res.writeHead(200, { 'Content-type': 'text/html; charset=utf-8' })
       res.write(data);
       res.end();
+
     }
-
-
   })
 
-
 });
+
+app.post('/location', (req, res) => {
+  const latitude = req.body.latitude;
+  const longitude = req.body.longitude;
+
+  // 위도와 경도 값을 이용한 로직을 구현할 수 있음
+
+  res.send('Location received');
+});
+
 
 app.listen(2080, (err) => {
 
