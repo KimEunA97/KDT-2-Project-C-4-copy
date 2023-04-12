3e03ee0a1bf2c787c165577ddb566389e695d5f4
KimEunA97 <kimeuna9712@gmail.com>
2023-04-12 11:59:21 +0900

    HTML 파일명 문제였따

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const app = express();
 app.get('/', function (req, res) {
 
 
-  fs.readFile('./index.html', function (err, data) {
+  fs.readFile('./show.html', function (err, data) {
 
     if (err) {
 
@@ -41,65 +41,4 @@ app.listen(2080, (err) => {
   }
 
 })
-
-
-// const server = http.createServer(function (request, response) {
-
-
-
-//   if (request.method === 'GET' && request.url === '/') {
-
-//     fs.readFile('./show.html', function (error, data) {
-
-//       if (error) {
-//         response.writeHead(404);
-//         console.error("에러!");
-//       }
-
-//       else {
-//         response.writeHead(200, { 'Content-type': 'text/html; charset=utf-8' });
-
-
-//         response.end(data);
-
-//         let buf = Buffer.from(data);
-//         console.log(buf);
-
-
-//         // let parseData = JSON.parse(buf);
-//         // console.log(parseData);
-
-//       }
-//     })
-
-//   }
-
-
-//   let body = "";
-//   if (request.method === 'POST' && request.url === '/location') {
-
-
-//     request.on('data', function (data) {
-
-//       body += data;
-
-//     });
-//     request.on('end', function () {
-//       console.log(body)
-//     })
-
-//   }
-
-
-// })
-
-// server.listen(3000, function (error) {
-
-//   if (error) {
-//     console.log("서버 구동 실패");
-//   }
-//   else {
-//     console.log("서버 구동");
-//   }
-// });
 
