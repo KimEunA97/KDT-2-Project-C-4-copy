20f21a82b48488ae865f6602d5d926b5d39de81b
jungheeeun <jungheeeun272@gmail.com>
2023-04-16 03:52:09 +0900

    url 정보 분석하기

--- web-Node/main.js
+++ web-Node/main.js
@@ -5,16 +5,24 @@ var app = http.createServer(function(request,response){
     var _url = request.url;//var url  // _url
     var queryData = url.parse(_url, true).query;
     var title = queryData.id; // 깔끔하게 보기위해서 
-    console.log(queryData.id);// HTML //css // JavaScript
+    // console.log(queryData.id); // HTML //css // JavaScript
     // console.log(_url);
-    if(_url == '/'){
-      // _url = '/index.html';    // templete 가져와서 필요 없음. 
-      title= "heen Homepage !";   // 최상의 경로 30일때 표시 . 
-    }
-    if(_url == '/favicon.ico'){
-      return response.writeHead(404);
-    }
-    response.writeHead(200);
+    //*rote 로 접했는가 아닌가 ? 파악하기 
+    console.log(url.parse(_url, true));//! 주어진 url정보를 분석
+    // Url {
+    //   protocol: null,
+    //   slashes: null,
+    //   auth: null,
+    //   host: null,
+    //   port: null,
+    //   hostname: null,
+    //   hash: null,
+    //   search: '?id=CSS',
+    //   query: [Object: null prototype] { id: 'CSS' },
+    //   pathname: '/',
+    //   path: '/?id=CSS',
+    //   href: '/?id=CSS'
+    // }
     fs.readFile(`data/${queryData.id}`, 'utf8', function(err, description){
       var templete = `
       <!doctype html>
@@ -35,7 +43,8 @@ var app = http.createServer(function(request,response){
   </body>
   </html>
       `;
-      //이제 본문 내용을 바꿔줘도 node main.js를 껐다가 켤 필요없이 본문내용이 업데이트 된다. 
+      //이제 본문 내용을 바꿔줘도 node main.js를 껐다가 켤 필요없이 본문내용이 업데이트 된다.  
+      response.writeHead(200);
       response.end(templete);
     })
     // console.log(__dirname + url);
@@ -45,4 +54,4 @@ var app = http.createServer(function(request,response){
 
  
 });
-app.listen(3050);
+app.listen(3000);
