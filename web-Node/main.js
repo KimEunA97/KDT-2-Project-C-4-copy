35b0c90367c27e21749796a45c0b7d4b652d04bb
jungheeeun <jungheeeun272@gmail.com>
2023-04-16 04:27:29 +0900

    조건문  - 홈페이지 구현

--- web-Node/main.js
+++ web-Node/main.js
@@ -5,11 +5,13 @@ var app = http.createServer(function(request,response){
     var _url = request.url;//var url  // _url
     var queryData = url.parse(_url, true).query;
     var pathname = url.parse(_url, true).pathname
-    var title = queryData.id; 
-
 
     if( pathname === "/"){
+      if(queryData.id === undefined){
+        
     fs.readFile(`data/${queryData.id}`, 'utf8', function(err, description){
+      var title = "heeun page"
+      var description = "good night"
       var templete = `
       <!doctype html>
   <html>
@@ -29,16 +31,38 @@ var app = http.createServer(function(request,response){
   </body>
   </html>
       `;
-      //이제 본문 내용을 바꿔줘도 node main.js를 껐다가 켤 필요없이 본문내용이 업데이트 된다.  
       response.writeHead(200);
       response.end(templete);
     });
+  }else{  
+    fs.readFile(`data/${queryData.id}`, 'utf8', function(err, description){
+      var title = queryData.id; 
+      var templete = `
+      <!doctype html>
+  <html>
+  <head>
+    <title>WEB1 - ${title}</title> 
+    <meta charset="utf-8">
+  </head>
+  <body>
+    <h1><a href="index.html">WEB</a></h1>
+    <ol>
+      <li><a href="/?id=HTML">HTML</a></li>
+      <li><a href="/?id=CSS">CSS</a></li>
+      <li><a href="/?id=JavaScript">JavaScript</a></li>
+    </ol>
+    <h2>${title}</h2>
+    <p>${description} </p>
+  </body>
+  </html>
+      `;  
+      response.writeHead(200);
+      response.end(templete);
+  });
+}
     }else{
       response.writeHead(404);
       response.end('not found');
     }
-
-
-
-});
+}); 
 app.listen(3000);
