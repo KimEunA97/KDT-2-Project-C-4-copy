14c6593510b08babab04209dbff5745bf4cc8427
jungheeeun <jungheeeun272@gmail.com>
2023-04-16 03:42:18 +0900

    이항 / 대입 / 비교 연산자 /boolean

---
+++ web-Node/syntax/boolean.js
@@ -0,0 +1,5 @@
+ //2개의 data 
+console.log(true);
+console.log(false);
+
+true =1;
---
+++ web-Node/syntax/comparison.js
@@ -0,0 +1,13 @@
+console.log(1+1); // 이항연산자
+//비교연산자
+console.log( 1 == 1 ); //true 
+console.log( 1 == 2 ) ; //false
+console.log ( 1>2 ) ; //false 
+
+console.log( 1 === 1) ; // true
+console.log( 1 === 2 ); // false 
+
+name = 1 // 대입연산자 
+name == 1 // 비교연산자 error 
+console.log(name); //1 
+
