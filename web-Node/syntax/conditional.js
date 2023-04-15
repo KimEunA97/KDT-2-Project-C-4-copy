c2a426dd4cbb4a7564e67ae1cb3bff87d08ceab5
jungheeeun <jungheeeun272@gmail.com>
2023-04-16 03:44:13 +0900

    조건문 시작

---
+++ web-Node/syntax/conditional.js
@@ -0,0 +1,22 @@
+var args = process.argv
+console.log(args);
+//' ~ web-Node/syntax/conditional.js'
+console.log(args[1]);
+console.log('A');
+console.log('B');
+if(args[1] == '1'){
+console.log('C1');
+}
+// if(false){
+  else{
+console.log('C2');
+}
+console.log('D');
+
+
+// INPUT
+// 입력을 부르는 여러가지 표현 
+// Parameter
+// Argument 
+
+// OUTPUT
---
+++ web-Node/syntax/program1.js
@@ -0,0 +1,10 @@
+console.log('A');
+console.log('B');
+console.log('C1');
+console.log('D');
+
+
+//주기성을 가진 기계적으로 하는 일들은 편하게 하고싶어짐.
+//program1,2에서 C1,C2 단하나만 바꾸고싶은데
+//바꿔야할 자료가 1억개라면?
+
