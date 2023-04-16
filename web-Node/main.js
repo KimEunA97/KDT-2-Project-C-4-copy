1629d7a5a152861716d572f962538634b5ba9d97
jungheeeun <jungheeeun272@gmail.com>
2023-04-16 13:23:37 +0900

    배열과 반복문을 이용해서 글목록 웹에 출력하기

--- web-Node/main.js
+++ web-Node/main.js
@@ -5,34 +5,44 @@ var app = http.createServer(function(request,response){
     var _url = request.url;//var url  // _url
     var queryData = url.parse(_url, true).query;
     var pathname = url.parse(_url, true).pathname
-
     if( pathname === "/"){
       if(queryData.id === undefined){
-        //만약 id 부분이 undefined 라면 ( home : 홈페이지 부분 ) 
-  
-      var title = "heeun page"
-      var description = "good night"
-      var templete = `
-      <!doctype html>
-  <html>
-  <head>
-    <title>WEB1 - ${title}</title> 
-    <meta charset="utf-8">
-  </head>
-  <body>
-    <h1><a href="index.html">WEB</a></h1>
-    <ol>
-      <li><a href="/?id=HTML">HTML</a></li>
-      <li><a href="/?id=CSS">CSS</a></li>
-      <li><a href="/?id=JavaScript">JavaScript</a></li>
-    </ol>
-    <h2>${title}</h2>
-    <p>${description} </p>
-  </body>
-  </html>
-      `;
-      response.writeHead(200);
-      response.end(templete);
+        fs.readdir('./data', function(error, filelist){
+          console.log(filelist);//[ 'CSS', 'HTML', 'JavaScript' ]
+          var title = "heeun page"
+          var description = "good night"
+        //   var list = `<ol>
+        //   <li><a href="/?id=HTML">HTML</a></li>
+        //   <li><a href="/?id=CSS">CSS</a></li>
+        //   <li><a href="/?id=JavaScript">JavaScript</a></li>
+        // </ol>`;
+          var list = '<ol>';
+          var i = 0 ;
+          while(i < filelist.length){
+            list = list + `<li>${filelist[i]}</li>`
+            i = i + 1;
+          }
+          list = list + '<ol>';
+
+          var templete = `
+          <!doctype html>
+      <html>
+      <head>
+        <title>WEB1 - ${title}</title> 
+        <meta charset="utf-8">
+      </head>
+      <body>
+        <h1><a href="index.html">WEB</a></h1>
+        ${list}
+        <h2>${title}</h2>
+        <p>${description} </p>
+      </body>
+      </html>
+          `;
+          response.writeHead(200);
+          response.end(templete); 
+        })  
+
   }else{  
     fs.readFile(`data/${queryData.id}`, 'utf8', function(err, description){
       var title = queryData.id; 
