9f529d92ae81123d126595a2c4962ace34b12d46
KimEunA97 <kimeuna9712@gmail.com>
2023-04-18 11:00:17 +0900

    date 객체로 20230418 이런식의 날짜로 문자열 변환해서 api호출 url에 대입하는 시도

---
+++ weatherAPI.js
@@ -0,0 +1,30 @@
+import { API_Key } from "./key.js";
+
+
+let url = `https://apis.data.go.kr/1360000/VilageFcstInfoService_2.0/getVilageFcst?${API_Key}&pageNo=1&numOfRows=1000&dataType=JSON&base_date=20230418&base_time=0500&nx=55&ny=127`
+
+console.log(url)
+
+let nx = "55";
+let ny = "127";
+
+// let todayDate = date;
+
+const dt = new Date();
+
+let year = dt.getFullYear();
+
+function getMonth(value) {
+  if (dt.getMonth() + 1 < 10) {
+
+    let act = dt.getMonth() + 1;
+    let td = "0"
+    let month = td + act;
+
+    console.log(month);
+    return month;
+  }
+
+}
+getMonth();
+console.log(month)
